58aa320afcda6623b30a768db648e6927e5d0f9c
Prakhar <prakharjoshi960@gmail.com>
2025-07-07 12:29:47 +0530

    Initial commit

---
+++ src/main/java/com/chat/app/model/ChatMessage.java
@@ -0,0 +1,24 @@
+package com.chat.app.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChatMessage {
+    private Long id;
+    private String sender;
+    private String content;
+    public String getSender() {
+        return sender;
+    }
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
